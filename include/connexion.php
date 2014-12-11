37019c8d03557e0a6dad6e3660a9756ccae4c47a
guibole93 <manzagregine@gmail.com>
2014-12-11 16:00:51 +0100

    Mise à jour connexion bdd
    Ajout d'une vérification du server name pour les identifiants à la base
    de données

--- include/connexion.php
+++ include/connexion.php
@@ -2,10 +2,18 @@
 
 function connexion_bdd()
 {
-	$bdd_serveur = 'localhost';
-	$bdd_nom = 'test';
-	$bdd_login = 'admin';
-	$bdd_mdp = '';
+
+	if($_SERVER["SERVER_NAME"] == "localhost") {
+		$bdd_serveur = 'localhost';
+		$bdd_nom = 'test';
+		$bdd_login = 'admin';
+		$bdd_mdp = '';
+	}else{
+		$bdd_serveur = 'db489144501.db.1and1.com';
+		$bdd_nom = 'db489144501';
+		$bdd_login = 'dbo489144501';
+		$bdd_mdp = 'axelyoyo86';
+	}
 
 	try{
 		$bdd = new PDO('mysql:host='.$bdd_serveur.';dbname='.$bdd_nom, $bdd_login, $bdd_mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
